c7591f287a047e929eb3152d34268ec26467af75
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-15 17:55:21 -0300

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import './app.css'
 import Home from "./pages/Home"
 
+import { useState } from 'react';
+
 import { Provider } from 'react-redux';
 import store from './store/store'
 
 import { BsMask } from 'react-icons/bs'
 
+
 function App() {
 
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   function toggle() {
-    var darkmode = document.getElementsByClassName("app")[0];
-    darkmode.style.filter = darkmode.style.filter ? "" : "invert(100%)";
+    setIsDarkMode(!isDarkMode);
+  
+    if (isDarkMode) {
+      var darkmodecontainer = document.getElementsByClassName("app")[0];
+      darkmodecontainer.style.filter = darkmodecontainer.style.filter ? "" : "invert(100%)";
+      document.body.style.backgroundColor = "black";
+    } else {
+      var darkmodecontainer = document.getElementsByClassName("app")[0];
+      darkmodecontainer.style.filter = darkmodecontainer.style.filter ? "" : "invert(100%)";
+      document.body.style.backgroundColor = "#f4f4f4";
+    }
   }
+  
 
   return (
     <Provider store={store}>
