543b134c26fcb11a4d217c505766163d1f11a373
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-16 09:39:44 -0300

    separação por divs

--- src/components/todos/index.jsx
+++ src/components/todos/index.jsx
@@ -1,6 +1,8 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { deleteTodo, toggleTodo } from '../../store/todosSlice'
 
+import { BsCheck2Circle, BsTrash3 } from 'react-icons/bs'
+
 const Todos = () => {
 
 
@@ -17,16 +19,23 @@ const Todos = () => {
 
 
     return (
-        <div>
+        <div className='todos'>
             <ul>
                 {todos.map(todo => (
                     <li
                         key={todo.id}
                         style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-                        onClick={() => handleToggleTodo(todo.id)}
                     >
-                        {todo.text}
-                        <button onClick={() => handleDeleteTodo(todo.id)}>Excluir</button>
+                        <div>
+                            <BsCheck2Circle onClick={() => handleToggleTodo(todo.id)}/>
+                        </div>
+                        <div>
+                            {todo.text}
+                        </div>
+                        <div>
+                            <BsTrash3 onClick={() => handleDeleteTodo(todo.id)}/>
+                        </div>
+                        
                     </li>
                 ))}
             </ul>
