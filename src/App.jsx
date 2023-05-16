1c4ce28794378fbdfcfda766d97dc8245c8f06eb
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-16 09:32:05 -0300

    correção de bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import './app.css'
-import Home from "./pages/Home"
-
-import { useState } from 'react';
-
+import './app.css';
+import Home from "./pages/Home";
+import { useState, useEffect } from 'react';
 import { Provider } from 'react-redux';
-import store from './store/store'
-
-import { BsMask } from 'react-icons/bs'
-
+import store from './store/store';
+import { BsMask } from 'react-icons/bs';
 
 function App() {
-
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  function toggle() {
-    setIsDarkMode(!isDarkMode);
-  
+  useEffect(() => {
     if (isDarkMode) {
       var darkmodecontainer = document.getElementsByClassName("app")[0];
       darkmodecontainer.style.filter = darkmodecontainer.style.filter ? "" : "invert(100%)";
@@ -25,17 +18,20 @@ function App() {
       darkmodecontainer.style.filter = darkmodecontainer.style.filter ? "" : "invert(100%)";
       document.body.style.backgroundColor = "#f4f4f4";
     }
+  }, [isDarkMode]);
+
+  function toggle() {
+    setIsDarkMode(!isDarkMode);
   }
-  
 
   return (
     <Provider store={store}>
       <div className="app">
         <Home />
-        <BsMask onClick={toggle} className="icon"/>
+        <BsMask onClick={toggle} className="icon" />
       </div>
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
